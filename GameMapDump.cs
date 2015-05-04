1d0c93aab316f68eced7854834d9557c675bb4d3
Matheus <matt.h.soares@gmail.com>
2015-05-04 14:19:39 -0300

    Added Filepath for dumping the assets

--- GameMapDump.cs
+++ GameMapDump.cs
@@ -1,8 +1,8 @@
 using StreamWriter = System.IO.StreamWriter;
- 
+
 using UnityEngine;
 using UnityEditor;
- 
+
 public static class GameMapDump
 {
     [MenuItem("Debug/Dump Scene")]
@@ -13,59 +13,67 @@ public static class GameMapDump
             Debug.LogError("Please select the object(s) you wish to dump.");
             return;
         }
- 
-		string filename = @"C:\Users\Mateus\Documents\Unity\SkyRidersEditor\Assets\island.txt";
- 
-        Debug.Log("Dumping scene to " + filename + " ...");
-        using (StreamWriter writer = new StreamWriter(filename, false))
+
+        var path = EditorUtility.SaveFilePanel("Save selected Objects", "", "island" + ".txt", "txt");
+
+        Debug.Log("Path :" + path);
+
+        if (path.Length != 0)
         {
-            foreach (GameObject gameObject in Selection.gameObjects)
+            Debug.Log("Dumping scene to " + path + " ...");
+            using (StreamWriter writer = new StreamWriter(path, false))
             {
-                DumpGameObject(gameObject, writer, "");
+                foreach (GameObject gameObject in Selection.gameObjects)
+                {
+                    DumpGameObject(gameObject, writer, "");
+                }
             }
+            Debug.Log("Scene dumped to " + path);
         }
-        Debug.Log("Scene dumped to " + filename);
     }
- 
+
     private static void DumpGameObject(GameObject gameObject, StreamWriter writer, string indent)
     {
-		MeshFilter mf = gameObject.GetComponent<MeshFilter>();
-		Transform t = gameObject.GetComponent<Transform>();
+        MeshFilter mf = gameObject.GetComponent<MeshFilter>();
+        Transform t = gameObject.GetComponent<Transform>();
 
-		string data = "";
-		if (mf)	data += "m ";	// Indica presenca de mesh
-		if (t && t.childCount == 0) { // Indica presenca de transform
-			data += "t";
-		} else {
-			t = null;
-		}
+        string data = "";
+        if (mf) data += "m ";	// Indica presenca de mesh
+        if (t && t.childCount == 0)
+        { // Indica presenca de transform
+            data += "t";
+        }
+        else
+        {
+            t = null;
+        }
 
-		string name = gameObject.name.Replace (" ", "");
+        string name = gameObject.name.Replace(" ", "");
 
-		writer.WriteLine("{0}+ {1} {2} !", indent, name, data);
-		//writer.WriteLine("{0}+'{1}' {2} !", indent, gameObject.name, data);
+        writer.WriteLine("{0}+ {1} {2} !", indent, name, data);
+        //writer.WriteLine("{0}+'{1}' {2} !", indent, gameObject.name, data);
 
-		DumpMesh (writer, indent+"  ", mf);
-		DumpTransform (writer, indent+"  ", t);
- 
+        DumpMesh(writer, indent + "  ", mf);
+        DumpTransform(writer, indent + "  ", t);
+
         foreach (Transform child in gameObject.transform)
         {
             DumpGameObject(child.gameObject, writer, indent + "  ");
         }
     }
 
-	private static void DumpMesh(StreamWriter writer, string indent, MeshFilter f)
-	{		
-		if (!f) return;
-		writer.WriteLine("{0} {1}", indent, f.sharedMesh.name);
-	}
+    private static void DumpMesh(StreamWriter writer, string indent, MeshFilter f)
+    {
+        if (!f) return;
+        writer.WriteLine("{0} {1}", indent, f.sharedMesh.name);
+    }
 
-	private static void DumpTransform(StreamWriter writer, string indent, Transform t)
-	{
-		if (!t) return;
-		writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}", indent,
-		                 t.position.x, t.position.y, t.position.z,
-		                 t.rotation.eulerAngles.x, t.rotation.eulerAngles.y, t.rotation.eulerAngles.z, 
-		                 t.lossyScale.x, t.lossyScale.y, t.lossyScale.z);
-	}
+    private static void DumpTransform(StreamWriter writer, string indent, Transform t)
+    {
+        if (!t) return;
+        writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}", indent,
+                         t.position.x, t.position.y, t.position.z,
+                         t.rotation.eulerAngles.x, t.rotation.eulerAngles.y, t.rotation.eulerAngles.z,
+                         t.lossyScale.x, t.lossyScale.y, t.lossyScale.z);
+    }
 }
