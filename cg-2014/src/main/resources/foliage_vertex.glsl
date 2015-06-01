c6d4254b7e08211a35962c4a10c46ce4654bbc60
MateusMP <mateus.mp93@gmail.com>
2015-06-01 05:41:51 -0300

    Awesome tree vertex shader

--- cg-2014/src/main/resources/foliage_vertex.glsl
+++ cg-2014/src/main/resources/foliage_vertex.glsl
@@ -32,25 +32,32 @@ out vec3 toLightVector;
 out vec3 toCameraVector;
 
 vec3 calculate_wind(vec3 pos){
-    float timeSec = u_time;
-    float sinTimeWind = sin(timeSec/120);
-    float sinTimeSec = sin(timeSec/30);
-
     vec3 u_pivot = vec3(0,0,0);
-    float factor = max(0.0, distance(u_pivot, pos)-14)*0.05;
-    float factorY = max(0.0, distance(u_pivot, pos)-20)*0.05;
+    float timeSec = u_time;
+    float distance = distance(u_pivot, pos);
+    float sinTimeWind = -clamp(pow(sin(cos(timeSec/120)*4),4),0,1)+0.8;//clamp(pow(sin(timeSec /500),64), -0.1, 2.0)+0.2;
+    float sinTimeSec = sinTimeWind*(cos(timeSec/2.5)/5+1); //sin(timeSec * distance/520);
+
+    float factor = max(0.0, distance-14)*0.3;
+    float factorY = max(0.0, distance-20)*0.05;
 
     vec3 wind = u_wind * sinTimeWind;
-    vec3 windSplash = vec3(-sinTimeSec,sinTimeSec,sinTimeSec);
+    vec3 windSplash = (vec3(-sinTimeSec,sinTimeSec,sinTimeSec)+wind)*sinTimeSec;
 
     return wind*factor + windSplash*factorY;
 }
 
 void main(void)
 {
+    mat4 rot = u_modelMatrix;
+    rot[3][0] = 0;
+    rot[3][1] = 0;
+    rot[3][2] = 0;
+    rot[3][3] = 1;
+
     // Apply wind
     vec3 wind = calculate_wind(a_position);
-    vec3 vpos = a_position + wind;
+    vec3 vpos = a_position + (rot*vec4(wind,0)).xyz;
     
     vec4 worldPosition = u_modelMatrix * vec4(vpos, 1.0);
     gl_Position = u_projectionMatrix * u_viewMatrix * worldPosition;
